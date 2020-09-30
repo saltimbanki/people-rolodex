c1897fb5137d89c9637d99efb223e0562ef8c9bb
saltimbanki <jorgebabarro@hotmail.com>
2020-09-30 07:48:22 +0200

    Updates

---
+++ precache-manifest.738d038e8576a2cb3f00120f7b075269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813a2e59ade1f88d960408671b180e9c",
+    "url": "/people-rolodex/index.html"
+  },
+  {
+    "revision": "4f311a69663dc21b9c60",
+    "url": "/people-rolodex/static/css/main.459e4d88.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/people-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/people-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f311a69663dc21b9c60",
+    "url": "/people-rolodex/static/js/main.21157ba8.chunk.js"
+  },
+  {
+    "revision": "e13a1ace5c92e5db4c90",
+    "url": "/people-rolodex/static/js/runtime-main.1db29647.js"
+  }
+]);
